d841918fab454921144a80b517b8635a8ebcd0a5
Christina Gonzalez <cmgamway@gmail.com>
2019-09-28 04:58:44 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,30 @@
+class Song
+  attr_accessor :name
+  attr_reader :artist, :song
+
+  @@all = []
+  def initialize(name, artist = nil, song = nil)
+    @name = name
+    @artist = artist
+    @song = song
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    all.clear
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create(name)
+    song = new(name)
+    song.save
+    song
+  end
+end
