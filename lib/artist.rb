fd4aba6c6457c1a011e8034171736bf75db2703b
Joseph Sesti <sestijoey63@gmail.com>
2019-09-29 19:58:48 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,38 @@
+class Artist
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.create(name)
+    @name = name
+    @@all << self
+    self
+  end
+  
+  def songs
+    @songs
+  end
+  
+  def add_song(song)
+    song.artist = self unless song.artist
+    songs << song unless songs.include?(song)
+  end
+end
---
+++ lib/genre.rb
@@ -0,0 +1,33 @@
+class Genre
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.create(name)
+    @name = name
+    @@all << self
+    self
+  end
+  
+  def songs
+    @songs
+  end
+end
