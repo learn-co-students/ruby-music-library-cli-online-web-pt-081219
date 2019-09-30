54de9dde4dd77cf283e31796a2acf31a627508eb
John Guest <jcguest90@gmail.com>
2019-09-30 05:15:27 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,20 @@ require 'bundler'
 Bundler.require
 
 module Concerns
+    module Findable
+
+        def find_by_name(name)
+            ## this works because its "extended" as a class method
+            all.detect{ |a| a.name == name }
+        end
+
+        def find_or_create_by_name(name)
+            find_by_name(name) || create(name)
+        end 
+
+    end
 end
+
+
 
 require_all 'lib'
