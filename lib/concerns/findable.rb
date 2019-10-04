106250c7341719ecc813a74eb22b39d704d40772
Esther Agyare-Boateng <eagyare91@gmail.com>
2019-10-04 23:19:03 +0000

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,5 +1,5 @@
-module 
-  Concerns::Findable
+module Concerns
+  module Findable
 
   def find_by_name(name)
     self.all.detect{|i| i.name == name}
@@ -8,5 +8,6 @@ module
   def find_or_create_by_name(name)
     self.find_by_name(name) || self.create(name)
   end
+end
 end
 
