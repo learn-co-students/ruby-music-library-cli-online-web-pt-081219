695b75f877aed1023c2e1415816f44f96cf4c6a8
Esther Agyare-Boateng <eagyare91@gmail.com>
2019-10-05 08:19:25 +0000

    Done.

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -5,4 +5,12 @@ class MusicImporter
     @path = path
   end
 
-end
+def path
+  @path
+  end
+  
+  def files
+    @files ||= Dir.glob("#{path}/*.mp3").collect {|file| file.gsub("#{path}/", "")
+    end
+  end 
+end 
